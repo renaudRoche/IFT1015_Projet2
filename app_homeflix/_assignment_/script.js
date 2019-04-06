549a05008ad6265fe460e3d5194e90c21ddd4cba
renaudRoche <46608222+renaudRoche@users.noreply.github.com>
2019-04-06 16:51:21 -0400

    Update script.js

--- app_homeflix/_assignment_/script.js
+++ app_homeflix/_assignment_/script.js
@@ -32,7 +32,7 @@ function searchMovies(movies,searchValue) {
 		if (movies[i].title.includes(searchValue))
 			listeFilms.push(movies[i]);
 	}
-	//console.log("listeFilms",listeFilms);
+	console.log("listeFilms",listeFilms);
 	return listeFilms;
 	
 } 
@@ -52,8 +52,8 @@ function sortMovies(movies, isAscending){
 
 
 function populateSelect(movies){
-	//console.log("parametre movies dans populateSelect");
-	//console.log(movies);
+	console.log("parametre movies dans populateSelect");
+	console.log(movies);
 	var listeGenre = [];
 	
 
@@ -63,16 +63,16 @@ function populateSelect(movies){
 				listeGenre.push(movies[i].genres[j])
 		}
 	}
-	//console.log(listeGenre);
+	console.log(listeGenre);
     return listeGenre; 
  }
 
 
 function filterMovies(movies, genreFilter ){
 	var listeFilms = [];
-	//console.log("parametre movies dans filterMovies");
-	//console.log(movies);
-	//console.log(genreFilter);
+	console.log("parametre movies dans filterMovies");
+	console.log(movies);
+	console.log(genreFilter);
 	for (var i= 0; i< movies.length; i++){
 		for (var j = 0; j< movies[i].genres.length;j++){
 			if (movies[i].genres.includes(genreFilter))
@@ -84,8 +84,60 @@ function filterMovies(movies, genreFilter ){
 
 }
 
+var SHOW_SAMPLE = true;
 
 function displayMovie(movie){
-	//console.log({title: movie.title,genres: movie.genres, element: movie.element});
-	//return {title: movie.title,genres: movie.genres, element: movie.element}
+	console.log({title: movie.title,genres: movie.genres, element: movie.element});
+	return {title: movie.title,genres: movie.genres, element: movie.element}
 }
+
+var element = createElement(li);
+	element.className = "c-main_item o-layout_item u-1/4";
+	var card = element.createElement(div);
+		card.className = "c-main_item_card";
+		var cover = card.createElement(div);
+			cover.className = "c-main_item_cover";// -red"
+			var preview = cover.createElement(p);
+				preview.className = "c-main_item_preview";
+				preview.innerHTML = //premiere lettre des titres
+			var details = cover.createElement(div);
+				details.className = "c-main_item_details";
+				var title = details.createElement(h3);
+					title.className = "c-main_item_header";
+					title.innerHTML = "Title :";
+					var titleText = title.createElement(p);
+						titleText.className = "c-main_item_text";
+						titleText.innerHTML = //titre du film
+				var genre = details.createElement(h3);
+					genre.className = "c-main_item_header";
+					genre.innerHTML = "Genre :";
+					var genreText = genre.createElement(p);
+						genreText.className = "c-main_item_text";
+						genreText.innerHTML = //genres du film
+				var synopsis = details.createElement(h3);<
+					synopsis.className = "c-main_item_header";
+					synopsis.innerHTML = "Synopsis :";
+					var synText = synopsis.createElement(p);
+						synText.className = "c-main_item_text";
+						synText.innerHTML = //synopsis du film
+				var length = details.createElement(h3);
+					length.className = "c-main_item_header";
+					length.innerHTML = "Length :";
+					var lengthText = length.createElement(span);
+						lengthText.className = "c-main_item_span";
+						lengthText.innerHTML = // movie length
+				var language = details.createElement(h3);
+					language.className = "c-main_item_header";
+					language.innerHTML = "Language :";
+					var languageText = language.createElement(span);
+						languageText.className = "c-main_item_span";
+						languageText.innerHTML = //langages du film
+				var rating = details.createElement(h3);
+					rating.className = "c-main_item_header";
+					rating.innerHTML = "Rating :";
+					var ratingText = rating.createElement(span);
+						ratingText.className = "c-main_item_span";
+						ratingText.innerHTML = // rating du film 
+		var subTitle = card.createElement(h2);
+			subTitle.className = "c-main_item_title";
+			subTitle.innerHTML = //titre du film
